11e6a7b425f2cd9c897ac74edbf01360971781b3
Joseph Lozano <joseph@hey.com>
2020-10-20 12:53:06 -0400

    resume

--- lib/joseph_lozano_web/endpoint.ex
+++ lib/joseph_lozano_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule JLWeb.Endpoint do
     at: "/",
     from: :joseph_lozano,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js resume.pdf favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
