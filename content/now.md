ac9737d09dd33dc6f6bd8f93766e8b9c64a5723e
Joseph Lozano <joseph@hey.com>
2020-10-20 14:51:26 -0400

    now and hello-blog

--- content/now.md
+++ content/now.md
@@ -1,9 +1,10 @@
 %{
   title: "Now",
+  description: "What I am up to right now",
   date: ~D[2020-10-19]
 }
 ---
 
 In between jobs. Starting something new November 2nd.
 
-In the meantime I am working on building my homepage.
+In the meantime I am working on building my homepage (where you are).
--- lib/joseph_lozano_web/controllers/article_controller.ex
+++ lib/joseph_lozano_web/controllers/article_controller.ex
@@ -20,13 +20,17 @@ defmodule JLWeb.ArticleController do
       "projects" ->
         render(conn, "projects.html", projects: Content.all_projects())
 
+      "now" ->
+        render(conn, "now.html", now: Content.get_article("now"))
+
       slug ->
         article = Content.get_article(slug)
 
         if is_nil(article) do
           render(conn, "not_found.html", slug: slug)
         else
-          render(conn, "article.html", article: article)
+          IO.inspect(article.description)
+          render(conn, "article.html", article: article, description: article.description)
         end
     end
   end
